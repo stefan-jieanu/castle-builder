a0460cb2fd2192fd3ecc84e44da1b54ec7ecdefc
stefan-jieanu <stefanalexjieanu@gmail.com>
2022-12-22 12:46:40 +0200

    removed useStrict

--- client/src/components/sketch/SketchCanvas.js
+++ client/src/components/sketch/SketchCanvas.js
@@ -1,56 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { memo, useEffect, useRef, useCallback } from 'react';
 import WebLily from 'weblily';
-// import { useDispatch } from 'react-redux';
-// import { create, reset } from '../../store/lilySlice';
 
-// export default function SketchCanvas() {
-//   const canvasRef = React.useRef(null);
+const SketchCanvas = memo(props => {
+  const canvasRef = useRef(null);
 
-//   React.useEffect(() => {
-//     console.log('Sketch canvas component mounted');
-
-//     const app = WebLily.create(canvasRef.current);
-//     app.start();
-
-//     return() => {
-//       // stop engine
-//       console.log('Sketch canvas component unmounted');
-//       app.stop();
-//     }
-//   }, );
-
-//   return (
-//     <canvas
-//       id="sketch-canvas"
-//       ref={canvasRef}
-//       style={{
-//         width: '100%',
-//         height: '100%'
-//       }}
-//       >
-//     </canvas>
-//   )
-// }
-
-const SketchCanvas = React.memo(props => {
-  const canvasRef = React.useRef(null);
-
-  React.useEffect(() => {
+  useEffect(() => {
     console.log('Sketch canvas component mounted');
 
     // createLilySketch(canvasRef);
     const app = WebLily.create(canvasRef.current);
     app.start();
 
+    const canvasElem = canvasRef.current;
+
     window.addEventListener('resize', app.resizeCallback.bind(app));
+    canvasElem.addEventListener('mousedown', app.mouseDownCallback.bind(app));
+
 
     return() => {
       // stop engine
       console.log('Sketch canvas component unmounted');
       app.stop();
     }
-  }, );
-
+  }, []);
+  
   return (
     <div id='canvas-wrapper'>
       <canvas
